e73bbb65409169e1d3a4161c1cc64513ca368710
farzanacoder <you@example.com>
2025-11-23 10:56:55 +0600

    designed

--- app/(Page)/Dashboard/page.tsx
+++ app/(Page)/Dashboard/page.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import SellsCard from '@/app/Components/DashCompo/SellsCard'
 
 const page = () => {
   return (
     <>
 
-    <div className="w-full h-screen bg-amber-600 flex justify-center items-center md:text-5xl text-sm font-bold ">
-      <h1>Deshboard page</h1>
-    </div>
-      
+      <div className="w-[1135px] h-screen flex justify-center items-center md:text-5xl text-sm font-bold ">
+        <SellsCard />
+        <SellsCard />
+        <SellsCard />
+        <SellsCard />
+
+      </div>
+
     </>
   )
 }
---
+++ app/Components/DashCompo/SellsCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BsThreeDotsVertical } from 'react-icons/bs'
+
+const SellsCard = () => {
+  return (
+    <div className='w-[265px] h-[108px] bg-white rounded-2xl'>
+        <div className='flex justify-between'>
+            <p className='text-[#7E7E8F]'>Total sells</p>
+            <BsThreeDotsVertical />
+        </div> 
+
+
+        <div></div>
+
+        <div></div>
+        
+    </div>
+  )
+}
+
+export default SellsCard
